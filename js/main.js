a879e36ced7ba2709af5b7d0b1f8da7a5d4ca36b
AzulChic <az.chicchon@gmail.com>
2015-11-06 00:34:11 -0500

    Chat Listo

--- js/main.js
+++ js/main.js
@@ -3,77 +3,10 @@
 $(document).ready(function() {
 		$("#enviar").click(function(e) {
 				e.preventDefault();
-				var mensaje = $("#mensaje").val();;
-				var elMensaje = $("<div></div>").text(mensaje).css("color", "#000").addClass("alert alert-success");
+				var color = $("#color").val()
+				var mensaje = $("#mensaje").val();
+
+				var elMensaje = $("<div></div>").text(mensaje).addClass("alert alert-"+color);
 					$(".ventana").append(elMensaje);
 			});
 });
-
-$(document).ready(function() {
-	$(".msg-ok").click(function(e) {
-		e.preventDefault();
-		var color = $(".msg-ok").val();
-		var valor = $("<p></p>").text(color).css("color", "#000");
-		$("#dropdownMenu2").text("ok");
-	});
-	$(document).ready(function() {
-		$("#enviar").click(function(e) {
-				e.preventDefault();
-				var mensaje = $("#mensaje").val();;
-				var elMensaje = $("<div></div>").text(mensaje).css("color", "#000").addClass("alert alert-success");
-					$(".ventana").append(elMensaje);
-			});
-});
-});
-
-$(document).ready(function() {
-		$(".msg-info").click(function(e) {
-		e.preventDefault();
-		var color = $(".msg-ok").val();
-		var valor = $("<p></p>").text(color).css("color", "#000");
-		$("#dropdownMenu2").text("info");
-	});
-		$(document).ready(function() {
-		$("#enviar").click(function(e) {
-				e.preventDefault();
-				var mensaje = $("#mensaje").val();;
-				var elMensaje = $("<div></div>").text(mensaje).css("color", "#000").addClass("alert alert-info");
-					$(".ventana").append(elMensaje);
-			});
-});
-});
-
-$(document).ready(function() {
-	$(".msg-warning").click(function(e) {
-		e.preventDefault();
-		var color = $(".msg-ok").val();
-		var valor = $("<p></p>").text(color).css("color", "#000");
-		$("#dropdownMenu2").text("warning");
-	});
-	$(document).ready(function() {
-		$("#enviar").click(function(e) {
-				e.preventDefault();
-				var mensaje = $("#mensaje").val();;
-				var elMensaje = $("<div></div>").text(mensaje).css("color", "#000").addClass("alert alert-warning");
-					$(".ventana").append(elMensaje);
-			});
-});
-});
-
-$(document).ready(function() {
-	$(".msg-danger").click(function(e) {
-		e.preventDefault();
-		var color = $(".msg-ok").val();
-		var valor = $("<p></p>").text(color).css("color", "#000");
-		$("#dropdownMenu2").text("danger");
-	});
-	$(document).ready(function() {
-		$("#enviar").click(function(e) {
-				e.preventDefault();
-				var mensaje = $("#mensaje").val();;
-				var elMensaje = $("<div></div>").text(mensaje).css("color", "#000").addClass("alert alert-danger");
-					$(".ventana").append(elMensaje);
-			});
-});
-});
-
